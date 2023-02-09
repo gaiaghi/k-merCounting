7f632a81e95ae301cf354e788d987d902150e4bb
Gaia Ghidoni <gaia.ghid@gmail.com>
2023-02-09 21:56:55 +0100

    cambio versioni build, spostato cartelle, stampa configurazione e veloce prova di counting

--- build.sbt
+++ build.sbt
@@ -1,11 +1,11 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.12.17"
+ThisBuild / scalaVersion := "2.13.10"
 
 lazy val root = (project in file("."))
   .settings(
     name := "k-merCounting"
   )
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "3.2.2"
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.2"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.3.1"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.3.1"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,5 @@
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.rdd.RDD
 import utils.SparkContextSetup
+
 
 object Main {
 
@@ -9,27 +8,63 @@ object Main {
     * args(0) = master
     * args(1) = path of the fasta file
     * args(2) = parallelism
+    * args(3) = execution mode (parallel / sequential)
+    * args(4) = canonical or non-canonical counting
     * */
 
-    //local only
-    System.setProperty("hadoop.home.dir", "C:\\Users\\Utente\\winutils\\hadoop-3.2.2")
+    //for local only
+//    System.setProperty("hadoop.home.dir", "C:\\Users\\Utente\\winutils\\hadoop-3.2.2")
 
     //read arguments
     val master = args(0)
     val fileName = if (args.length > 1) args(1) else "data/humantest.fna"
-    val parallelism = if (args.length >2) args(2) else "1"
+    val parallelism = if (args.length > 2) args(2) else "1"
+    val exeMode = if (args.length > 3 && args(3) == "parallel") args(3) else "sequential"
+    val countingType = if (args.length > 4 && args(4) == "canonical") args(4) else "non-canonical"
 
     //creating spark session
-//    val sparkconf = new SparkConf().setAppName("k-mer Counting").setMaster(master)
-//    val sparkContext = new SparkContext(sparkconf)
     val sparkSession = SparkContextSetup.sparkSession(master, parallelism.toInt)
     val sparkContext = sparkSession.sparkContext
+
+    //chosed configuration
+    println("Current configuration:")
+    println("\t- master: " + master)
+    println("\t- data path: " + fileName)
+    println("\t- counting type: " + countingType)
+    println("\t- execution mode: "+ exeMode)
+    println("\t- Spark Context initialized with parallelism: " + parallelism)
 
 
     //loading the fasta file
     val genSeq = sparkContext.textFile(fileName)
 
-    println(genSeq)
+    
+    //prova veloce
+    // ___________________________________________________________
+    val input = "data/sample.fna"
+    val K = 2
+    val broadcastK = sparkContext.broadcast(K)
+    val records = sparkContext.textFile(input)
+    // remove the records, which are not an actual sequence data
+
+    val filteredRDD = records.filter(line => {
+      !(
+        line.startsWith("@") ||
+          line.startsWith("+") ||
+          line.startsWith(";") ||
+          line.startsWith(">")
+        )
+    })
+    filteredRDD.collect().foreach(println)
+    val kmers = filteredRDD.flatMap(_.sliding(broadcastK.value, 1).map((_, 1)))
+    kmers.collect().foreach(println)
+
+    // find frequencies of kmers
+    val kmersGrouped = kmers.reduceByKey(_ + _)
+
+    kmersGrouped.saveAsTextFile("output1.txt")
+
+    //___________________________________________________________
 
   }
 }
